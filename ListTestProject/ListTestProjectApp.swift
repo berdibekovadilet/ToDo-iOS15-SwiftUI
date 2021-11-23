82ff446b2554f30c26c6a71de680ad3547dde35e
Adilet <berdibekovadilet@icloud.com>
2021-11-24 00:04:48 +0300

    Initial Commit

---
+++ ListTestProject/ListTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListTestProjectApp.swift
+//  ListTestProject
+//
+//  Created by Adilet Berdibekov on 24.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct ListTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
